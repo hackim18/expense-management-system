e9af0f37a9a2c6b73addd27161e556bd06072d0c
Hackim18 <khakim668@gmail.com>
2025-12-25 19:07:59 +0700

    refactor(backend): refactoring code common route

--- backend/internal/delivery/http/route/common_route.go
+++ backend/internal/delivery/http/route/common_route.go
@@ -13,47 +13,44 @@ import (
 func (c *RouteConfig) RegisterApiRoutes(api *gin.RouterGroup) {
 	startTime := time.Now()
 	openAPIPath := "api/openapi.yaml"
-	welcomeHandler := func(ctx *gin.Context) {
-		res := gin.H{"message": messages.WelcomeMessage}
-		ctx.JSON(http.StatusOK, res)
-	}
 
-	healthHandler := func(ctx *gin.Context) {
-		res := gin.H{"status": "ok"}
-		ctx.JSON(http.StatusOK, res)
-	}
-
-	metricsHandler := func(ctx *gin.Context) {
-		uptime := time.Since(startTime).String()
-		res := gin.H{"status": "ok", "uptime": uptime}
-		ctx.JSON(http.StatusOK, res)
-	}
-
-	api.GET("", welcomeHandler)
-	api.GET("/health", healthHandler)
+	api.GET("", c.welcomeHandler())
+	api.GET("/health", c.healthHandler())
 	api.GET("/openapi.yaml", func(ctx *gin.Context) {
 		ctx.File(openAPIPath)
 	})
-	api.GET("/metrics", metricsHandler)
+	api.GET("/metrics", c.metricsHandler(startTime))
 }
 
 func (c *RouteConfig) RegisterPublicRoutes() {
-	welcomeHandler := func(ctx *gin.Context) {
-		res := gin.H{"message": messages.WelcomeMessage}
-		ctx.JSON(http.StatusOK, res)
-	}
-
-	healthHandler := func(ctx *gin.Context) {
-		res := gin.H{"status": "ok"}
-		ctx.JSON(http.StatusOK, res)
-	}
-
 	app := c.Router
-	app.GET("/", welcomeHandler)
-	app.GET("/health", healthHandler)
+	app.GET("/", c.welcomeHandler())
+	app.GET("/health", c.healthHandler())
 	app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, ginSwagger.URL("/api/openapi.yaml")))
 	app.NoRoute(func(ctx *gin.Context) {
 		res := gin.H{"message": messages.NotFound}
 		ctx.AbortWithStatusJSON(http.StatusNotFound, res)
 	})
+}
+
+func (c *RouteConfig) welcomeHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		res := gin.H{"message": messages.WelcomeMessage}
+		ctx.JSON(http.StatusOK, res)
+	}
+}
+
+func (c *RouteConfig) healthHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		res := gin.H{"status": "ok"}
+		ctx.JSON(http.StatusOK, res)
+	}
+}
+
+func (c *RouteConfig) metricsHandler(startTime time.Time) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		uptime := time.Since(startTime).String()
+		res := gin.H{"status": "ok", "uptime": uptime}
+		ctx.JSON(http.StatusOK, res)
+	}
 }
